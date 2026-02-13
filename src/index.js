f8a25bea397caf42f354216d2b6c1ab43c55119d
iWhatty <25625546+iWhatty@users.noreply.github.com>
2026-02-13 15:39:00 -0500

    Added more types to index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@
 export { HermesHandler } from "./HermesHandler.js";
 
 /**
- * @typedef {import("./HermesHandler.js").HermesMessage} HermesMessage
- * @typedef {import("./HermesHandler.js").HermesResponse<any>} HermesResponse
+ * Public type exports (JSDoc re-exports).
+ * These show up in the generated .d.ts when using `declaration` + `allowJs`.
  */
 
+/**
+ * @typedef {import("./HermesHandler.js").HermesOk<any>} HermesOk
+ * @typedef {import("./HermesHandler.js").HermesErr} HermesErr
+ * @typedef {import("./HermesHandler.js").HermesResponse<any>} HermesResponse
+ * @typedef {import("./HermesHandler.js").HermesMessage} HermesMessage
+ * @typedef {import("./HermesHandler.js").HermesContext} HermesContext
+ * @typedef {import("./HermesHandler.js").HermesHandlerFn} HermesHandlerFn
+ * @typedef {import("./HermesHandler.js").HermesLogger} HermesLogger
+ */
